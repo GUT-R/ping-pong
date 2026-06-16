5cf4f9b60c0aeaff99b4bb495e2a4f6cc3fa508c
Augusto borges <139978458+GUTR3213X@users.noreply.github.com>
2026-06-16 12:03:09 +0000

    Increasing buffer optimizations with more C

--- almighty/src/main.pyx
+++ almighty/src/main.pyx
@@ -9,25 +9,23 @@ cdef struct Rect:
     int_fast8_t h
     int_fast8_t x
     
-cdef void f_cursor_pos(char* buf, int offset, int x, int y, const char* s):
+cdef void f_cursor_pos(char* buf, int offset, int x, int y, const char* s) nogil:
     sprintf(buf + offset, "\033[%d;%dH%s",
                                  y, x, s # type: ignore
             ) 
 
 # p_iter_size: position iterable size
-cdef char* f_cursor_positions(int[2]* positions, size_t p_iter_size, const char* fill):
+cdef char* f_cursor_positions(int[2]* positions, size_t p_iter_size, const char* fill) nogil:
     # Considerando que cada ANSI pode ter no máximo 13 caracteres de tamanho
     # Se tiver mais, o código quebra.
     cdef const size_t fill_size = strlen(fill)
     cdef char* buf = <char*> malloc((0x0D + fill_size) * p_iter_size)
-    cdef const int offset = 0
     cdef int i
 
     for i in range(p_iter_size):
-        f_cursor_pos(buf, offset, 
+        f_cursor_pos(buf, 13 * i,
             positions[i][0], positions[i][1] # type: ignore
         )
-        offset += 0x0D
 
     return buf
 
@@ -87,32 +85,43 @@ cdef class Display:
     cdef public int w
     cdef public int h
     cdef public str bkg
+    cdef public int ch_size
     cdef public list matrix # type: ignore
-    _cleaned_positions: list[tuple[int, int]]
-    _drawed_positions: list[tuple[int, int]]
+    cdef (int[2], char*)* _cleaned_positions
+    cdef (int[2], char*)* _drawed_positions
     def __init__(self, w: int, h: int, background_char: str) -> None:
         self.w = w
         self.h = h
         self.bkg = background_char
+        self.ch_size = len(self.bkg)
         self.matrix: list[list[str]] = [[background_char] * w] * h # Acabei aprendendo isso na prova de Minora (https://github.com/leonardo-minora)
-        self._cleaned_positions = []
-        self._drawed_positions = []
+        self.reset_buffer()
+    cdef reset_buffer(self):
+        self._cleaned_positions = <(int[2]*, char*)*> malloc(<size_t> w * h * (sizeof(int) * 2 + sizeof(char) * self.ch_size))
+        self._drawed_positions = <(int[2]*, char*)*> malloc(<size_t> w * h * (sizeof(int) * 2 + sizeof(char) * self.ch_size))
     
     cdef clear(self, rect: GraphicRect):
-        self._cleaned_positions.clear()
+        """WARNING: Reset buffer before runnning this."""
+        cdef int k = 0;
         for i in range(rect.y, rect.y + rect.h):
             for j in range(rect.x, rect.x + rect.w):
                 if self.matrix[i][j] != self.bkg:
-                    self.matrix[i][j] = self.bkg # type: ignore
-                    self._cleaned_positions.append((j, i))
+                    self.matrix[i][j] = self.bkg                                     # type: ignore
+                    self._cleaned_positions[k][0] = j                                # type: ignore
+                    self._cleaned_positions[k][1] = i                                # type: ignore
+                    self._cleaned_positions[k][2] = self.matrix[i][j][:self.ch_size] # type: ignore
+                    k += 1
     
     cdef draw(self, GraphicRect rect):
-        self._drawed_positions.clear()
+        cdef int k = 0;
         for i in range(rect.y, rect.y + rect.h):
             for j in range(rect.x, rect.x + rect.w):
-                if self.matrix[i][j] != rect.repr:
-                    self.matrix[i][j] = rect.repr # type: ignore
-                    self._drawed_positions.append((j, i))
+                if self.matrix[i][j] != self.bkg:
+                    self.matrix[i][j] = self.bkg                                    # type: ignore
+                    self._drawed_positions[k][0] = j                                # type: ignore
+                    self._drawed_positions[k][1] = i                                # type: ignore
+                    self._drawed_positions[k][2] = self.matrix[i][j][:self.ch_size] # type: ignore
+                    k += 1
     cpdef str render(self):
         return '\n'.join(map(''.join, self.matrix))
 
