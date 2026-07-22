380b2d8893b43eee071bff1b96973bd5838a5163
Augusto Borges <139978458+GUT-R@users.noreply.github.com>
2026-07-22 20:37:29 -0300

    Update author name in setup.py

--- almighty/setup.py
+++ almighty/setup.py
@@ -4,6 +4,6 @@ from Cython.Build import cythonize
 setup(
     name="The Almighty",
     ext_modules=cythonize("src/main.pyx"),
-    author="GUTR3213X",
+    author="GUT-R",
     author_email="augusto.borges@academico.ifrn.edu.br",
 )
